1942b07a589bda3b844bf5a0a51a744f8b2b2158
TJ Palanca <mail@tjpalanca.com>
2021-05-15 06:33:46 +0000

    Start migration environment

---
+++ R/dbtools-package.R
@@ -0,0 +1,10 @@
+#' @import logger
+#' @importFrom R6 R6Class
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
